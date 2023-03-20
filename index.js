068e93275d3da85f92bae82970e3bcf9d9281959
Wilfred <wilcr20@gmail.com>
2023-03-19 18:05:42 -0600

    Subiendo APi para LectorTmo

--- index.js
+++ index.js
@@ -286,7 +286,6 @@ app.get('/manga/tmomanga/home', function (_, res) {
                     mangaList.push(manga);
                 }
             });
-            console.log(mangaList.length)
             res.send({ data: mangaList });
         }
         else {
@@ -417,14 +416,100 @@ app.post('/manga/tmomanga/searchByGenre', function (req, res) {
 
 // lectorTmo
 
-app.get('/manga/lectortmo/home', function (_, response) {
-    cloudscraper.get('https://lectortmo.com/populars').then((data) => {
-        response.send(data)
+app.get('/manga/lectortmo/home', function (_, res) {
+    cloudscraper.get('https://lectortmo.com/populars').then((body) => {
+        var $ = cheerio.load(body);
+        let listItems = $("div.element");
+        var mangaList = [];
+        if (listItems.length > 60) {
+            listItems = listItems.slice(0, 59);
+        }
+        listItems.each((_idx, el) => {
+            const manga = { title: "", imageUrl: "", mangaUrl: "", website: "lectortmo" };
+            manga.mangaUrl = $(el).find("a").attr("href").trim();
+            manga.title = $(el).find("h4").text().trim().replace(/\n/g, '');
+            manga.imageUrl = $(el).find("style").text().trim().replace(/\n/g, '').split("('")[1].split("')")[0];
+            mangaList.push(manga);
+        });
+        res.send({ data: mangaList });
     }, (err) => {
         response.send(err)
     })
-
 });
+
+
+app.get('/manga/lectortmo/trends', function (_, res) {
+    cloudscraper.get('https://lectortmo.com/').then((body) => {
+        var $ = cheerio.load(body);
+        let listItems = $("div.element");
+        var mangaList = [];
+        if (listItems.length >= 60) {
+            listItems = listItems.slice(36, 48);
+        }
+        listItems.each((_idx, el) => {
+            const manga = { title: "", imageUrl: "", mangaUrl: "", website: "lectortmo" };
+            manga.mangaUrl = $(el).find("a").attr("href").trim();
+            manga.title = $(el).find("h4").text().trim().replace(/\n/g, '');
+            manga.imageUrl = $(el).find("style").text().trim().replace(/\n/g, '').split("('")[1].split("')")[0];
+            mangaList.push(manga);
+        });
+        res.send({ data: mangaList });
+    }, (err) => {
+        response.send(err)
+    })
+});
+
+
+app.post('/manga/lectortmo/mangaInfo', function (req, res) {
+    cloudscraper.get(req.body.mangaUrl).then((body) => {
+        var $ = cheerio.load(body);
+        const mangaInfo = { title: "", description: "", imageUrl: "", genreList: [], chapterList: [], state: "", website: "lectortmo" }
+        mangaInfo.title = $(".element-subtitle").eq(0).text();
+        mangaInfo.description = $(".element-description").eq(0).text().trim();
+        mangaInfo.imageUrl = $(".book-thumbnail ").eq(0).attr("src");
+        mangaInfo.state = $(".book-status.publishing").text().trim();
+
+        var chapterListHtml = $("ul.list-group.list-group-flush li.upload-link");
+        chapterListHtml.each((_idx, el) => {
+            mangaInfo.chapterList.push({
+                chapter: $(el).find(".btn-collapse").text().trim(),
+                chapterUrl: $(el).find(".text-right a").eq(0).attr("href")
+            });
+        });
+        var genreListHtml = $("h6 .badge");
+        genreListHtml.each((_idx, el) => {
+            mangaInfo.genreList.push({ genre: $(el).text().trim() });
+        });
+        res.send(mangaInfo)
+    }, (err) => {
+        response.send(err)
+    })
+});
+
+
+app.post('/manga/lectortmo/search', function (req, res) {
+    // req.body.term
+    console.log("https://lectortmo.com/library?_pg=1&title=" + req.body.term);
+    cloudscraper.get("https://lectortmo.com/library?_pg=1&title=" + req.body.term).then((body) => {
+        var $ = cheerio.load(body);
+        let listItems = $("div.element");
+        var mangaList = [];
+        if (listItems.length > 60) {
+            listItems = listItems.slice(0, 59);
+        }
+        listItems.each((_idx, el) => {
+            const manga = { title: "", imageUrl: "", mangaUrl: "", website: "lectortmo" };
+            manga.mangaUrl = $(el).find("a").attr("href").trim();
+            manga.title = $(el).find("h4").text().trim().replace(/\n/g, '');
+            manga.imageUrl = $(el).find("style").text().trim().replace(/\n/g, '').split("('")[1].split("')")[0];
+            mangaList.push(manga);
+        });
+        res.send({ data: mangaList });
+    }, (err) => {
+        response.send(err)
+    });
+})
+
 
 
 var server = app.listen(3000, function () {
